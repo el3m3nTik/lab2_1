15f95412ee187db87b7af76ae6f23732dba85441
el3m3nTiC <90087363+el3m3nTik@users.noreply.github.com>
2022-02-05 10:46:04 +0200

    add lab2_1

---
+++ lab2_1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <chrono>
+using namespace std;
+class Timer
+{
+private:
+	using clock_t = std::chrono::high_resolution_clock;
+	using second_t = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<clock_t> m_beg;
+
+public:
+	Timer() : m_beg(clock_t::now())
+	{
+	}
+
+	void reset()
+	{
+		m_beg = clock_t::now();
+	}
+
+	double elapsed() const
+	{
+		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+	}
+};
+uint64_t factorial(int n)
+{
+	uint64_t F = 1;
+	for (int i = 1; i <= n; i++)
+		F *= i;
+	return F;
+
+}
+int main()
+{
+	int n;
+	cin >> n;
+	Timer t;
+	cout << factorial(n) << endl;
+	cout << "Time elapsed:" << t.elapsed() << endl;
+}
